049e5c6edca1e40ca565ec1eca4a0deab3bb5d19
Ivan Malijkh <i.malijkh@gmail.com>
2012-10-04 12:53:42 +0200

    VCR back to testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,17 @@ require File.expand_path('../../lib/wishlist.rb', __FILE__)
 require 'minitest/autorun'
 require 'webmock/minitest'
 require 'turn'
+require 'vcr'
 
 Turn.config do |c|
   c.format = :outline
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/wishlist_cassettes'
+  c.hook_into :webmock
+  
+  c.ignore_request do |req|
+    URI(req.uri).query == "/wlmapi/2.0/json/auth"
+  end
 end
