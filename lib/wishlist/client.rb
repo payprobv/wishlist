da6cd0792dd5ed5172d490ee648d86f1a3e2e3fc
Ivan Malijkh <i.malijkh@gmail.com>
2012-10-04 12:56:34 +0200

    caching of the endpoints disabled

--- lib/wishlist/client.rb
+++ lib/wishlist/client.rb
@@ -9,46 +9,47 @@ module Wishlist
     end
     
     def levels
-      @levels ||= Wishlist::Level.new(authentication)
+      @levels = Wishlist::Level.new(authentication)
     end
     
     def level_members(options)
-      @level_members ||= Wishlist::LevelMember.new(authentication, options)
+      puts "level_members: #{options.inspect}"
+      @level_members = Wishlist::LevelMember.new(authentication, options)
     end
     
     
     def posts(options)
-      @posts ||= Wishlist::Post.new(authentication, options)
+      @posts = Wishlist::Post.new(authentication, options)
     end
     
     def pages(options)
-      @pages ||= Wishlist::Page.new(authentication, options)
+      @pages = Wishlist::Page.new(authentication, options)
     end
     
     def comments(options)
-      @comments ||= Wishlist::Comment.new(authentication, options)
+      @comments = Wishlist::Comment.new(authentication, options)
     end
     
     def categories(options)
-      @categories ||= Wishlist::Category.new(authentication, options)
+      @categories = Wishlist::Category.new(authentication, options)
     end
     
     
     def protected_posts
-      @protected_posts ||= Wishlist::ProtectedPost.new(authentication)
+      @protected_posts = Wishlist::ProtectedPost.new(authentication)
     end
     
     def protected_pages
-      @protected_pages ||= Wishlist::ProtectedPage.new(authentication)
+      @protected_pages = Wishlist::ProtectedPage.new(authentication)
     end
     
     def protected_categories
-      @protected_categories ||= Wishlist::ProtectedCategory.new(authentication)
+      @protected_categories = Wishlist::ProtectedCategory.new(authentication)
     end
     
     
     def members
-      @members ||= Wishlist::Member.new(authentication)
+      @members = Wishlist::Member.new(authentication)
     end
     
   end
